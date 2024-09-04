4df6f1627ec5ef1ee84fc80eb56f38527556fbe4
Marcelo Miranda <78736175+ticoxz@users.noreply.github.com>
2024-09-03 23:02:07 -0400

    test nocturno

--- components/Auth/Auth.js
+++ components/Auth/Auth.js
@@ -30,56 +30,69 @@ const Auth = () => {
 
   useEffect(() => {
     // Esperar hasta que todo haya cargado completamente antes de iniciar la autenticación
-    window.onload = () => {
+    const handleAuthorization = () => {
       authorize();
     };
+
+    if (document.readyState === "complete") {
+      handleAuthorization();
+    } else {
+      window.addEventListener("load", handleAuthorization);
+    }
+
+    return () => {
+      window.removeEventListener("load", handleAuthorization);
+    };
   }, []);
+
 
   const authorize = async () => {
     const baseUrl = 'https://tucar-auth-13535404425.us-central1.run.app/api/v1/oauth/authorize';
     const params = {
-      response_type: 'code', // Aquí va el tipo de respuesta que esperas
+      response_type: 'code',
       client_id: 'QT6xCtFyNRNPSsopvf4gbSxhPgxuzV3at4JoSg0YG7s',
-      redirect_uri: 'http://localhost:3000', // Cambia esto al URI de redirección deseado
-      scope: 'driver', // Alcance de la solicitud
+      redirect_uri: 'http://localhost:3000',
+      scope: 'driver',
       state: 'random-state',
       tenancy: 'development'
     };
   
     const queryString = new URLSearchParams(params).toString();
     const fullUrl = `${baseUrl}?${queryString}`;
-  
     console.log('URL completa para autorización:', fullUrl);
 
     // Redirigir automáticamente a la URL construida
     window.history.pushState(null, '', `/?${queryString}`);
 
     try {
-      const response = await axios.get(fullUrl, {
-        withCredentials: true
-      });
-  
-      const data = response.data;
-      console.log('Respuesta completa del servidor:', data);
-  
-      if (data.authMethods && data.authMethods.length > 0) {
-        setAuthMethods(data.authMethods);
+      const response = await axios.get(fullUrl, { withCredentials: true });
+
+      if (response.status === 200) {
+        const data = response.data;
+        console.log('Respuesta completa del servidor:', data);
+
+        if (data.authMethods && data.authMethods.length > 0) {
+          setAuthMethods(data.authMethods);
+        } else {
+          console.error('authMethods no está presente en la respuesta o está vacío');
+        }
+        
+        setAuthSessionId(data.authSessionId);
+        localStorage.setItem('authSessionId', data.authSessionId);
+        setAuthFlow(data.authFlow);
+        setCompleted(data.completed);
+        setAuthData(data.authData);
+
+        updateFingerprint(data.authSessionId);
       } else {
-        console.error('authMethods no está presente en la respuesta o está vacío');
+        console.error('Error en la autorización: ', response.status);
       }
-  
-      setAuthSessionId(data.authSessionId);
-      localStorage.setItem('authSessionId', data.authSessionId);
-      setAuthFlow(data.authFlow);
-      setCompleted(data.completed);
-      setAuthData(data.authData);
-  
-      // Actualizar fingerprint después de recibir authSessionId
-      updateFingerprint(data.authSessionId);
     } catch (error) {
-      console.error('Error en la autorización', error);
+      console.error('Error en la autorización:', error);
+      setErrorMessage('Error al intentar autorizar. Verifique su conexión.');
     }
   };
+
 
   const updateFingerprint = async (authSessionId) => {
     try {
@@ -147,11 +160,18 @@ const Auth = () => {
         {errorMessage && (
           <div className="text-center py-5 bg-red-400 p-3">{errorMessage}</div>
         )}
-        {!authSessionId ? (
-          <p>Cargando...</p> // Indicador de carga mientras se obtiene el authSessionId
-        ) : (
-          <AuthForm />
-        )}
+      {!authSessionId ? (
+        <div>
+          <p>Cargando...</p>
+          {errorMessage && (
+            <div className="text-red-500 mt-2">
+              {errorMessage}
+            </div>
+          )}
+        </div>
+      ) : (
+        <AuthForm />
+      )}
       </div>
     </AuthProvider>
   );
