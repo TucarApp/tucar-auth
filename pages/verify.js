2896e50ec94b8d3b4e7c49e6f4a3b182fc0c3873
Tico Miranda <ticomiranda@MacBook-Pro-de-Tico.local>
2024-09-23 16:32:21 -0300

    test

--- pages/verify.js
+++ pages/verify.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useAuthContext } from '../components/Auth/AuthProvider';
 
-// Estilo para el modal (puedes ajustarlo como desees)
 const VerifyContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -38,17 +37,15 @@ const Verify = () => {
   const [redirectUri, setRedirectUri] = useState(''); // Estado para almacenar la URL de redirección
   const [secondsLeft, setSecondsLeft] = useState(6); // Estado para el temporizador de 6 segundos
 
-  // Hacer el `verifyAuthentication` desde `/verify`
   useEffect(() => {
     const handleVerifyAuthentication = async () => {
       try {
-        // Llama a `verifyAuthentication` y obtén el redirectUri
         const response = await verifyAuthentication(authSessionId);
         const redirectUri = response?.redirectUri;
 
         if (redirectUri) {
-          setRedirectUri(redirectUri); // Almacenar la URL de redirección
-          setIsVerifying(false); // Finalizamos la verificación
+          setRedirectUri(redirectUri);
+          setIsVerifying(false);
         } else {
           setErrorMessage('No se encontró una URL de redirección.');
         }
@@ -58,26 +55,21 @@ const Verify = () => {
       }
     };
 
-    // Llamamos a la función para hacer la verificación
     if (authSessionId && udiFingerprint && state) {
       handleVerifyAuthentication();
     }
   }, [authSessionId, udiFingerprint, state, verifyAuthentication]);
 
-  // Redirigir automáticamente a `redirectUri` después de la verificación
   useEffect(() => {
     if (!isVerifying && redirectUri) {
-      // Iniciar temporizador de 6 segundos y actualizar texto cada segundo
       const timer = setInterval(() => {
         setSecondsLeft((prev) => prev - 1);
       }, 1000);
 
-      // Redirigir después de 6 segundos
       const redirectTimeout = setTimeout(() => {
         router.push(redirectUri);
-      }, 6000); // Redirigir después de 6 segundos
+      }, 6000);
 
-      // Limpiar temporizadores al desmontar el componente
       return () => {
         clearInterval(timer);
         clearTimeout(redirectTimeout);
@@ -85,7 +77,6 @@ const Verify = () => {
     }
   }, [isVerifying, redirectUri, router]);
 
-  // Función para redirigir inmediatamente cuando el botón es clickeado
   const handleImmediateRedirect = () => {
     if (redirectUri) {
       router.push(redirectUri);
@@ -109,5 +100,9 @@ const Verify = () => {
     </VerifyContainer>
   );
 };
+
+export async function getServerSideProps() {
+  return { props: {} };
+}
 
 export default Verify;
