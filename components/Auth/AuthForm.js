f744861248fa6702ae5b955548b4fe87bbb174bb
Tico Miranda <ticomiranda@MacBook-Pro-de-Tico.local>
2024-09-23 17:44:27 -0300

    test upe

--- components/Auth/AuthForm.js
+++ components/Auth/AuthForm.js
@@ -82,6 +82,7 @@ const AuthForm = () => {
   const [code3, setCode3] = useState("");
   const [code4, setCode4] = useState("");
   const [inputError, setInputError] = useState(false);
+  const [passwordWarning, setPasswordWarning] = useState(""); // Advertencia de contraseña
 
   // Limpiar el error cuando el usuario comience a escribir en email o teléfono
   useEffect(() => {
@@ -94,6 +95,7 @@ const AuthForm = () => {
   useEffect(() => {
     if (code1 || code2 || code3 || code4) {
       setInputError(false);
+      setErrorMessage(""); // Limpiar el mensaje de error cuando el código se introduce correctamente
     }
   }, [code1, code2, code3, code4]);
 
@@ -110,7 +112,11 @@ const AuthForm = () => {
   const handleInputChange = (setCode, nextInputId) => (e) => {
     const value = e.target.value.replace(/\D/, ""); // Permitir solo números
     setCode(value);
-    if (value && nextInputId) {
+    
+    // Si se completa el último campo, automáticamente hace submit
+    if (!nextInputId && value.length === 1 && code1 && code2 && code3 && code4) {
+      handleVerificationSubmit(); // Llamar al submit cuando todos los campos están completos
+    } else if (value && nextInputId) {
       const nextInput = document.getElementById(nextInputId);
       if (nextInput) {
         nextInput.focus();
@@ -130,6 +136,7 @@ const AuthForm = () => {
   const handleVerificationSubmit = () => {
     if (code1 && code2 && code3 && code4) {
       setInputError(false);
+      setErrorMessage(""); // Limpiar el error al hacer submit
       if (isGoogleFlow) {
         submitAuthenticationGoogle();
       } else {
@@ -142,10 +149,20 @@ const AuthForm = () => {
 
   const handleSubmit = async () => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,}$/;
+  
+    // Validación de contraseña con mensaje de advertencia
+    if (currentStep === 4) {
+      if (!passwordRegex.test(password)) {
+        setPasswordWarning("La contraseña debe tener al menos 8 caracteres, una letra mayúscula, un número y un símbolo especial.");
+        return;
+      } else {
+        setPasswordWarning(""); // Limpiar la advertencia si la validación es correcta
+      }
+    }
   
     // Si el flujo es de Google, omitir la validación de emailOrPhone
     if (!isGoogleFlow) {
-      // Validación de correo electrónico
       if (!emailOrPhone) {
         setInputError("Por favor, completa el campo o inicia sesión con Google o Uber.");
         return;
@@ -170,16 +187,6 @@ const AuthForm = () => {
       setInputError("Error en la autenticación. Por favor, inténtalo de nuevo.");
     }
   };
-  
-
-  // const handleSubmit = async () => {
-  //   if (isGoogleFlow) {
-  //     await submitAuthenticationGoogle();
-  //   } else {
-  //     await submitAuthentication();
-  //   }
-  // };
-  
 
   return (
     <div className="text-center">
@@ -207,10 +214,9 @@ const AuthForm = () => {
               <InputField
                 type="text"
                 value={emailOrPhone}
-                onChange={(e) => setEmailOrPhone(e.target.value)}
+                onChange={(e) => setEmailOrPhone(e.target.value.toLowerCase())} // Convertir a minúsculas
               />
               <div className="flex flex-col justify-center items-center">
-                {/* Mostrar mensaje de error encima del botón "Continuar" */}
                 {(inputError || errorMessage) && (
                   <p className="text-red-500 text-sm mt-5 font-Poppins font-light">
                     {inputError || errorMessage}
@@ -266,181 +272,177 @@ const AuthForm = () => {
         </FormContainer>
       )}
 
-{currentStep === 2 && (
-  <div className=" w-full h-full flex justify-center items-center mt-[80px] pantallapc:mt-[110px]">
-    <div className="w-[355px]">
-      <div className="flex justify-start">
-        <p className="text-[28px] font-semibold text-[#0057b8]">¡Hola!</p>
-      </div>
-      <div className="flex justify-start">
-        <p className="text-[16px] font-Poppins font-semibold text-[#0057b8]">
-          Regístrate para comenzar
-        </p>
-      </div>
-      <div className="pt-[20px]">
-        <p className="flex justify-start text-[14px] font-medium font-Poppins text-[#5b5d71] mb-[-5px]">
-          Nombre
-        </p>
-        <InputField
-          type="text"
-          value={firstname}
-          onChange={(e) => setFirstname(e.target.value)}
-        />
-      </div>
-      <div className="mt-5">
-        <p className="flex justify-start text-[14px] font-medium font-Poppins text-[#5b5d71] mb-[-5px]">
-          Apellido
-        </p>
-        <InputField
-          type="text"
-          value={lastname}
-          onChange={(e) => setLastname(e.target.value)}
-        />
-      </div>
-      <div className="mt-5">
-        <p className="flex justify-start text-[14px] font-medium font-Poppins text-[#5b5d71] mb-[-5px]">
-          Correo electrónico
-        </p>
-        <InputField
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="mt-5">
-        <p className="flex justify-start text-[14px] font-medium font-Poppins text-[#5b5d71] mb-[-5px]">
-          Teléfono
-        </p>
-        <InputField
-          type="text"
-          value={phone}
-          onChange={handlePhoneChange}
-        />
-      </div>
-      {/* Mostrar error si hay campos faltantes */}
-      {errorMessage && (
-        <p className="text-red-500 text-sm mt-5 font-Poppins font-light">
-          {errorMessage}
-        </p>
-      )}
-      <AuthButton onClick={handleSubmit}>
-        <p className="font-Poppins font-medium text-[#5b5d71]">Continuar</p>
-      </AuthButton>
-    </div>
-  </div>
-)}
-
-
-{currentStep === 3 && (
-  <div className="w-full h-full flex justify-center items-center mt-[80px] pantallapc:mt-[110px]">
-    <div className="w-[355px]">
-      <div className="flex flex-col items-start">
-        <p className="text-[28px] text-left font-Poppins font-semibold text-[#0057b8]">
-          Código de <span className="">verificación</span>
-        </p>
-      </div>
-      <div className="flex justify-start">
-        <p className="text-[16px] font-Poppins font-medium text-[#0057b8] text-left">
-          Ingresa el código que hemos enviado al número de teléfono
-        </p>
-      </div>
-      <div className="flex gap-x-[25px] lg:w-[350px]">
-        <VerificatonInput
-          id="input1"
-          type="text"
-          maxLength={1}
-          value={code1}
-          onChange={handleInputChange(setCode1, "input2")}
-          pattern="\d*"
-          className={`font-Poppins font-medium ${inputError ? "border-red-500" : ""}`}
-        />
-        <VerificatonInput
-          id="input2"
-          type="text"
-          maxLength={1}
-          value={code2}
-          onChange={handleInputChange(setCode2, "input3")}
-          pattern="\d*"
-          className={`font-Poppins font-medium ${inputError ? "border-red-500" : ""}`}
-        />
-        <VerificatonInput
-          id="input3"
-          type="text"
-          maxLength={1}
-          value={code3}
-          onChange={handleInputChange(setCode3, "input4")}
-          pattern="\d*"
-          className={`font-Poppins font-medium ${inputError ? "border-red-500" : ""}`}
-        />
-        <VerificatonInput
-          id="input4"
-          type="text"
-          maxLength={1}
-          value={code4}
-          onChange={handleInputChange(setCode4, null)}
-          pattern="\d*"
-          className={`font-Poppins font-medium ${inputError ? "border-red-500" : ""}`}
-        />
-      </div>
-
-      {inputError && (
-        <div className="text-red-500 mt-2">
-          *Por favor completa todos los campos.
-        </div>
-      )}
-
-      {/* Mostrar error específico */}
-      {errorMessage && (
-        <div className="text-red-500 mt-2">
-          {errorMessage}
-        </div>
-      )}
-
-      <AuthButton onClick={handleVerificationSubmit}>
-        <p className="font-Poppins font-medium">Continuar</p>
-      </AuthButton>
-    </div>
-  </div>
-)}
-
-
-
-
-
-{currentStep === 4 && (
-  <div className="w-full h-full flex justify-center items-center mt-[80px] pantallapc:mt-[110px]">
-    <div className="w-[350px]">
-      <div className="flex justify-start">
-        <p className="text-[28px] text-center font-Poppins font-semibold text-[#0057b8]">
-          Ingresa tu <span className="">Contraseña</span>
-        </p>
-      </div>
-      <div className="mt-5">
-        <p className="font-Poppins font-medium text-[14px] text-[#5b5d71] mb-[-10px] text-start">
-          Ingresar Contraseña
-        </p>
-        <InputField
-          type="password"
-          placeholder=""
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-            setErrorMessage(''); // Limpiar el mensaje de error cuando el usuario escribe
-          }}
-        />
-        {errorMessage && (
-          <div className="text-red-500 mt-2">
-            {errorMessage}
+      {currentStep === 2 && (
+        <div className=" w-full h-full flex justify-center items-center mt-[80px] pantallapc:mt-[110px]">
+          <div className="w-[355px]">
+            <div className="flex justify-start">
+              <p className="text-[28px] font-semibold text-[#0057b8]">¡Hola!</p>
+            </div>
+            <div className="flex justify-start">
+              <p className="text-[16px] font-Poppins font-semibold text-[#0057b8]">
+                Regístrate para comenzar
+              </p>
+            </div>
+            <div className="pt-[20px]">
+              <p className="flex justify-start text-[14px] font-medium font-Poppins text-[#5b5d71] mb-[-5px]">
+                Nombre
+              </p>
+              <InputField
+                type="text"
+                value={firstname}
+                onChange={(e) => setFirstname(e.target.value)}
+              />
+            </div>
+            <div className="mt-5">
+              <p className="flex justify-start text-[14px] font-medium font-Poppins text-[#5b5d71] mb-[-5px]">
+                Apellido
+              </p>
+              <InputField
+                type="text"
+                value={lastname}
+                onChange={(e) => setLastname(e.target.value)}
+              />
+            </div>
+            <div className="mt-5">
+              <p className="flex justify-start text-[14px] font-medium font-Poppins text-[#5b5d71] mb-[-5px]">
+                Correo electrónico
+              </p>
+              <InputField
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value.toLowerCase())} // Convertir a minúsculas
+              />
+            </div>
+            <div className="mt-5">
+              <p className="flex justify-start text-[14px] font-medium font-Poppins text-[#5b5d71] mb-[-5px]">
+                Teléfono
+              </p>
+              <InputField
+                type="text"
+                value={phone}
+                onChange={handlePhoneChange}
+              />
+            </div>
+            {errorMessage && (
+              <p className="text-red-500 text-sm mt-5 font-Poppins font-light">
+                {errorMessage}
+              </p>
+            )}
+            <AuthButton onClick={handleSubmit}>
+              <p className="font-Poppins font-medium text-[#5b5d71]">Continuar</p>
+            </AuthButton>
           </div>
-        )}
-        <AuthButton onClick={handleSubmit}>
-          <p className="font-Poppins font-medium">Registrarse</p>
-        </AuthButton>
-      </div>
-    </div>
-  </div>
-)}
+        </div>
+      )}
 
+      {currentStep === 3 && (
+        <div className="w-full h-full flex justify-center items-center mt-[80px] pantallapc:mt-[110px]">
+          <div className="w-[355px]">
+            <div className="flex flex-col items-start">
+              <p className="text-[28px] text-left font-Poppins font-semibold text-[#0057b8]">
+                Código de <span className="">verificación</span>
+              </p>
+            </div>
+            <div className="flex justify-start">
+              <p className="text-[16px] font-Poppins font-medium text-[#0057b8] text-left">
+                Ingresa el código que hemos enviado al número de teléfono
+              </p>
+            </div>
+            <div className="flex gap-x-[25px] lg:w-[350px]">
+              <VerificatonInput
+                id="input1"
+                type="text"
+                maxLength={1}
+                value={code1}
+                onChange={handleInputChange(setCode1, "input2")}
+                pattern="\d*"
+                className={`font-Poppins font-medium ${inputError ? "border-red-500" : ""}`}
+              />
+              <VerificatonInput
+                id="input2"
+                type="text"
+                maxLength={1}
+                value={code2}
+                onChange={handleInputChange(setCode2, "input3")}
+                pattern="\d*"
+                className={`font-Poppins font-medium ${inputError ? "border-red-500" : ""}`}
+              />
+              <VerificatonInput
+                id="input3"
+                type="text"
+                maxLength={1}
+                value={code3}
+                onChange={handleInputChange(setCode3, "input4")}
+                pattern="\d*"
+                className={`font-Poppins font-medium ${inputError ? "border-red-500" : ""}`}
+              />
+              <VerificatonInput
+                id="input4"
+                type="text"
+                maxLength={1}
+                value={code4}
+                onChange={handleInputChange(setCode4, null)} // Null para el último campo
+                pattern="\d*"
+                className={`font-Poppins font-medium ${inputError ? "border-red-500" : ""}`}
+              />
+            </div>
+
+            {inputError && (
+              <div className="text-red-500 mt-2">
+                *Por favor completa todos los campos.
+              </div>
+            )}
+
+            {errorMessage && (
+              <div className="text-red-500 mt-2">
+                {errorMessage}
+              </div>
+            )}
+
+            <AuthButton onClick={handleVerificationSubmit}>
+              <p className="font-Poppins font-medium">Continuar</p>
+            </AuthButton>
+          </div>
+        </div>
+      )}
+
+      {currentStep === 4 && (
+        <div className="w-full h-full flex justify-center items-center mt-[80px] pantallapc:mt-[110px]">
+          <div className="w-[350px]">
+            <div className="flex justify-start">
+              <p className="text-[28px] text-center font-Poppins font-semibold text-[#0057b8]">
+                Ingresa tu <span className="">Contraseña</span>
+              </p>
+            </div>
+            <div className="mt-5">
+              <p className="font-Poppins font-medium text-[14px] text-[#5b5d71] mb-[-10px] text-start">
+                Ingresar Contraseña
+              </p>
+              <InputField
+                type="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                  setErrorMessage(''); // Limpiar el mensaje de error cuando el usuario escribe
+                }}
+              />
+              {errorMessage && (
+                <div className="text-red-500 mt-2">
+                  {errorMessage}
+                </div>
+              )}
+              {passwordWarning && (
+                <div className="text-red-500 mt-2">
+                  {passwordWarning}
+                </div>
+              )}
+              <AuthButton onClick={handleSubmit}>
+                <p className="font-Poppins font-medium">Registrarse</p>
+              </AuthButton>
+            </div>
+          </div>
+        </div>
+      )}
 
       {currentStep === 5 && (
         <div className="w-full h-full flex justify-center items-center mt-[80px] pantallapc:mt-[110px]">
@@ -451,23 +453,19 @@ const AuthForm = () => {
                 <span className="block">cuenta de Uber</span>
               </p>
             </div>
-            {currentStep === 5 && (
-              <div>
-                <div className="flex justify-center items-center mt-4">
-                  <UberButton onClick={handleUberLogin}>
-                    <img
-                      src="uberlog.png"
-                      alt="Uber Logo"
-                      width={18}
-                      className="ml-[-15px]"
-                    />
-                    <span className="font-Poppins font-normal">
-                      Continuar con Uber
-                    </span>
-                  </UberButton>
-                </div>
-              </div>
-            )}
+            <div className="flex justify-center items-center mt-4">
+              <UberButton onClick={handleUberLogin}>
+                <img
+                  src="uberlog.png"
+                  alt="Uber Logo"
+                  width={18}
+                  className="ml-[-15px]"
+                />
+                <span className="font-Poppins font-normal">
+                  Continuar con Uber
+                </span>
+              </UberButton>
+            </div>
           </div>
         </div>
       )}
@@ -503,5 +501,3 @@ const AuthForm = () => {
 };
 
 export default AuthForm;
-
-
