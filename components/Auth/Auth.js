dc183af6bfa489bcbd2fdaa642f3efb3ab780f44
Tico Miranda <ticomiranda@MacBook-Pro-de-Tico.local>
2024-09-26 17:05:41 -0300

    marce

--- components/Auth/Auth.js
+++ components/Auth/Auth.js
@@ -56,63 +56,63 @@ const Auth = () => {
   }, [searchParams]);
 
   // Función para la autorización inicial
-  const authorize = async () => {
-    const baseUrl = 'https://tucar-auth-13535404425.us-central1.run.app/api/v1/oauth/authorize';
-  
-    // Capturar los parámetros de la URL o usar valores por defecto
-    const responseType = searchParams.get('response_type') || 'code';
-    const stateParam = searchParams.get('state') || 'random-state';
-    const clientId = searchParams.get('client_id') || 'QT6xCtFyNRNPSsopvf4gbSxhPgxuzV3at4JoSg0YG7s';
-    const redirectUri = searchParams.get('redirect_uri') || 'http://localhost:3000';
-    const scope = searchParams.get('scope') || 'driver';
-    const tenancy = 'production';
-  
-    const params = {
-      response_type: responseType,
-      client_id: clientId,
-      redirect_uri: redirectUri,
-      scope: scope,
-      state: stateParam,
-      tenancy: tenancy
-    };
-  
-    const queryString = new URLSearchParams(params).toString();
-    const fullUrl = `${baseUrl}?${queryString}`;
-  
-    console.log('Llamando a authorize con la URL completa:', fullUrl);
-  
-    try {
-      const response = await axios.get(fullUrl, {
-        withCredentials: true
-      });
-  
-      const data = response.data;
-  
-      // Verifica si hay métodos de autenticación en la respuesta
-      if (data.authMethods && data.authMethods.length > 0) {
-        setAuthMethods(data.authMethods);
-  
-        // Recorrer todos los authMethods y hacer console.log del methodType
-        data.authMethods.forEach((method, index) => {
-          console.log(`AuthMethod ${index + 1}:`, method.methodType);
-        });
-      } else {
-        console.error('authMethods no está presente en la respuesta o está vacío');
-      }
-  
-      setAuthSessionId(data.authSessionId);
-      localStorage.setItem('authSessionId', data.authSessionId);
-      setAuthFlow(data.authFlow);
-      setCompleted(data.completed);
-      setAuthData(data.authData);
-  
-      // Actualizar fingerprint después de recibir authSessionId
-      updateFingerprint(data.authSessionId);
-    } catch (error) {
-      console.error('Error en la autorización', error);
-    }
+const authorize = async () => {
+  const baseUrl = 'https://tucar-auth-13535404425.us-central1.run.app/api/v1/oauth/authorize';
+
+  // Capturar los parámetros de la URL o usar valores por defecto
+  const responseType = searchParams.get('response_type') || 'code';
+  const stateParam = searchParams.get('state') || 'random-state';
+  const clientId = searchParams.get('client_id') || 'QT6xCtFyNRNPSsopvf4gbSxhPgxuzV3at4JoSg0YG7s';
+  const redirectUri = searchParams.get('redirect_uri') || 'http://localhost:3000';
+  const scope = searchParams.get('scope') || 'driver';
+  const tenancy = 'production';
+
+  const params = {
+    response_type: responseType,
+    client_id: clientId,
+    redirect_uri: redirectUri,
+    scope: scope,
+    state: stateParam,
+    tenancy: tenancy
   };
-  
+
+  const queryString = new URLSearchParams(params).toString();
+  const fullUrl = `${baseUrl}?${queryString}`;
+
+  console.log('Llamando a authorize con la URL completa:', fullUrl);
+
+  try {
+    const response = await axios.get(fullUrl, {
+      withCredentials: true
+    });
+
+    const data = response.data;
+
+    // Verifica si hay métodos de autenticación en la respuesta
+    if (data.authMethods && data.authMethods.length > 0) {
+      setAuthMethods(data.authMethods);
+
+      // Recorrer todos los authMethods y hacer console.log del methodType
+      data.authMethods.forEach((method, index) => {
+        console.log(`AuthMethod ${index + 1}:`, method.methodType);
+      });
+    } else {
+      console.error('authMethods no está presente en la respuesta o está vacío');
+    }
+
+    setAuthSessionId(data.authSessionId);
+    localStorage.setItem('authSessionId', data.authSessionId);
+    setAuthFlow(data.authFlow);
+    setCompleted(data.completed);
+    setAuthData(data.authData);
+
+    // Actualizar fingerprint después de recibir authSessionId
+    updateFingerprint(data.authSessionId);
+  } catch (error) {
+    console.error('Error en la autorización', error);
+  }
+};
+
 
   // Actualizar el fingerprint después de la autorización
   const updateFingerprint = async (authSessionId) => {
