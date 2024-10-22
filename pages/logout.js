05317ef08140c6843353d425afffc5cbc578919b
Marcelo Miranda <78736175+ticoxz@users.noreply.github.com>
2024-10-22 17:19:40 -0300

    test

--- pages/logout.js
+++ pages/logout.js
@@ -4,24 +4,30 @@ import { useRouter } from 'next/router';
 const Logout = () => {
   const router = useRouter();
 
+  const tucar = 'https://tucar.app'
+
+  console.log('logout')
   useEffect(() => {
     // Llamar a la API de logout para eliminar la cookie
     const logoutUser = async () => {
+        
       try {
+        console.log('borrando')
         await fetch('/api/logout', {
           method: 'POST',
         });
-
+        console.log('esto es en el await')
         // Redirigir al home después de eliminar la cookie
-        router.push('https://tucar.app');
+        router.push(tucar);
       } catch (error) {
         console.error('Error durante el logout:', error);
       } 
+
     };
 
     logoutUser();
   }, [router]);
-
+  console.log(tucar)
   return (
     <div className="flex justify-center items-center h-screen">
       <p className="text-lg">Cerrando sesión...</p>
