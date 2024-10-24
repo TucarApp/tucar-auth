7368fe533ba54519ab31c70bb0c088120b5b2d47
Marcelo Miranda <78736175+ticoxz@users.noreply.github.com>
2024-10-24 11:45:57 -0300

    dev

--- pages/profile.js
+++ pages/profile.js
@@ -1,23 +1,39 @@
-import { ChevronRightIcon } from '@heroicons/react/solid'; // Asegúrate de instalar este paquete si aún no lo tienes
+import { ChevronRightIcon } from '@heroicons/react/solid'; 
+import { EyeIcon, EyeOffIcon } from '@heroicons/react/outline'; // Para los íconos de mostrar/ocultar contraseña
 import { useState } from 'react';
 import TucarLogo from '../components/LogoTucar/LogoTucar';
 import AuthButton from '@/components/Auth/AuthButton';
 import InputField from '@/components/Auth/InputField';
+
 const Cuenta = () => {
     const [selectedSection, setSelectedSection] = useState('Account Info');
     const [isEditing, setIsEditing] = useState(false);
     const [name, setName] = useState('Marcelo Miranda');
     const [phone, setPhone] = useState('+56 9 88973679');
     const [email, setEmail] = useState('ticomiranda4@gmail.com');
+    const [isPasswordEditing, setIsPasswordEditing] = useState(false); 
+    const [password, setPassword] = useState('********');
+    const [showPassword, setShowPassword] = useState(false); // Controla si la contraseña se muestra o no
 
     // Función para cambiar entre secciones
     const handleSectionChange = (section) => {
         setSelectedSection(section);
     };
 
-    // Función para guardar los cambios
-    const handleSave = () => {
-        setIsEditing(false);
+    // Función para habilitar la edición de la contraseña
+    const handlePasswordClick = () => {
+        setIsPasswordEditing(true);
+    };
+
+    // Función para guardar la nueva contraseña
+    const handlePasswordSave = () => {
+        setIsPasswordEditing(false);
+        // Lógica para guardar la contraseña en el backend
+    };
+
+    // Función para alternar la visibilidad de la contraseña
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
     };
 
     return (
@@ -65,7 +81,6 @@ const Cuenta = () => {
                             </div>
 
                             <h3 className="text-lg font-semibold mb-3">Mi perfil</h3>
-                            {/* Información del usuario */}
                             <div className="mb-5">
                                 <p className="text-gray-700">Nombre</p>
                                 {isEditing ? (
@@ -106,7 +121,6 @@ const Cuenta = () => {
                                 )}
                             </div>
 
-                            {/* Botón para habilitar/deshabilitar la edición */}
                             {isEditing ? (
                                 <AuthButton
                                     onClick={handleSave}
@@ -129,15 +143,45 @@ const Cuenta = () => {
                     {selectedSection === 'Security' && (
                         <div className="text-[#333333]">
                             <h2 className="text-2xl font-bold mb-5">Ajustes de Seguridad</h2>
-                            <div className="flex justify-between items-center">
-                                <div>
+                            <div className="flex justify-between items-center cursor-pointer" onClick={handlePasswordClick}>
+                                <div className="relative">
                                     <p className="font-medium text-lg mb-1">Contraseña</p>
-                                    <p className='font-medium'>********</p>
-                                   
+                                    {isPasswordEditing ? (
+                                        <div className="relative">
+                                            <InputField
+                                                type={showPassword ? 'text' : 'password'} // Alternar entre 'password' y 'text'
+                                                className="border border-gray-300 rounded p-2 w-full"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                            />
+                                            <div
+                                                className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer pt-[15px]"
+                                                onClick={togglePasswordVisibility} // Lógica para alternar el tipo de input
+                                            >
+                                                {showPassword ? (
+                                                    <EyeOffIcon className="h-5 w-5 text-gray-500" />
+                                                ) : (
+                                                    <EyeIcon className="h-5 w-5 text-gray-500" />
+                                                )}
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <p className="font-medium">********</p>
+                                    )}
+                                  
                                 </div>
                                 <ChevronRightIcon className="w-5 h-5 text-gray-400" />
                             </div>
                             <hr className="my-4" />
+
+                            {isPasswordEditing && (
+                                <AuthButton
+                                    onClick={handlePasswordSave}
+                                    className="bg-blue-500 text-white px-4 py- rounded"
+                                >
+                                    Guardar Contraseña
+                                </AuthButton>
+                            )}
                         </div>
                     )}
 
